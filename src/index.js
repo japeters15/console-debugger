498b423dd71f47640d709cff28f923326c54ee83
Will Berlanga <wberlanga@Wills-iMac.local>
2017-03-17 09:08:38 -0400

    Added Mocha verification.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/**
+ * Overrides console.log message.
+ * Adds UTC timestamp to log event.
+ * Suppresses console.log messages if LOG_DEBUG is false.
+ * Writes values to server.log file in logs/ at directory root.
+ * @param {string} msg
+ */
+
+// Assign console to new Console object.
+const Console = console;
+// imports values from .env file
+require('dotenv').config();
+// provides access to the file system
+const fs = require('fs');
+// creates log_file instance at the file path
+const logFile = fs.createWriteStream('logs/server.log', { flags: 'w' });
+
+// sets current UTC timestamp
+const timestamp = new Date().toISOString();
+// logs server start with timestamp
+logFile.write(timestamp + ' Server started.\n');
+// logs value .env debug value
+logFile.write('Logging set to DEBUG -- ' + process.env.LOG_DEBUG + '\n');
+
+// overrides console.log
+Console.log = (msg) => {
+  // checks to see if .env debug value is set to true
+  if (process.env.LOG_DEBUG === 'true') {
+    // writes log message to log with timestamp
+    logFile.write(new Date().toISOString() + ' :: ' + msg + '\n');
+  }
+};
---
+++ test/sanity_check.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+// Test suite
+describe('Mocha Sanity Check', () => {
+    // Test spec (unit)
+  it('it should run our tests using npm', () => {
+    expect(true).to.be.ok;
+  });
+});
